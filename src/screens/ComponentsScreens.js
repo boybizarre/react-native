5b0b0715727d4973269c6b8feb45fb5abb6dbaf6
boybizarre <106738064+boybizarre@users.noreply.github.com>
2023-10-17 17:55:41 +0100

    rules of jsx

---
+++ src/screens/ComponentsScreens.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const ComponentsScreen = () => {
+
+  const greeting = <Text>Hello to you</Text>
+
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is the components screen</Text>
+      {greeting}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+  },
+});
+
+export default ComponentsScreen;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>Hi There! My name is Jamal!</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
+
